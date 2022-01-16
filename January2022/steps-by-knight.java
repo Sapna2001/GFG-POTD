fa72bfde3b8ffb5f6faaf31489cd7cef766fbe92
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2022-01-16 17:56:23 +0530

    day16

--- January2022/steps-by-knight.java
+++ January2022/steps-by-knight.java
@@ -1,8 +1,7 @@
 /*
 Given a square chessboard, the initial position of Knight and position of a target. Find out the minimum steps a Knight will take to reach the target position.
 
-Note:
-The initial and the target position co-ordinates of Knight have been given accoring to 1-base indexing.
+Note: The initial and the target position co-ordinates of Knight have been given accoring to 1-base indexing.
 
 Link: https://practice.geeksforgeeks.org/problems/steps-by-knight5927/1#
 */
